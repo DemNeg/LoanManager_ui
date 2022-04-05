54c7dd25b37061baa27541e648e259515b26a5d4
DemNeg <basingoudembele@gmail.com>
2022-04-05 18:46:01 +0200

    UDATE LOANS-ROUTING.MODULE
    - Now we have loans-routing module updated with its      children routes.

--- src/app/loans/loans-routing.module.ts
+++ src/app/loans/loans-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddLoansComponent } from './add-loans/add-loans.component';
+import { DeleteLoansComponent } from './delete-loans/delete-loans.component';
+import { EditLoansComponent } from './edit-loans/edit-loans.component';
+import { ListLoansComponent } from './list-loans/list-loans.component';
 import { LoansComponent } from './loans.component';
+import { SearchLoansComponent } from './search-loans/search-loans.component';
 
-const routes: Routes = [{ path: '', component: LoansComponent }];
+const routes: Routes = [
+  { path: '', component: ListLoansComponent},
+  { path: 'add', component: AddLoansComponent},
+  { path: 'edit/:id', component: EditLoansComponent},
+  { path: 'delete/:id', component: DeleteLoansComponent},
+  { path: 'search', component: SearchLoansComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
