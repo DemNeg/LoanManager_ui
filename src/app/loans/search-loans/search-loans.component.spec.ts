acd22cb6066470c9a1a9e99b04be2f007d035596
DemNeg <basingoudembele@gmail.com>
2022-04-05 19:05:47 +0200

    CREATE CHILDREN CMPT FOR LOANS
    - Now we have children components responsible for crud operation on loans and search

---
+++ src/app/loans/search-loans/search-loans.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchLoansComponent } from './search-loans.component';
+
+describe('SearchLoansComponent', () => {
+  let component: SearchLoansComponent;
+  let fixture: ComponentFixture<SearchLoansComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchLoansComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchLoansComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
