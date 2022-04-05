0f225892aa46f973a861cc6ecc71780000f7a2e5
DemNeg <basingoudembele@gmail.com>
2022-04-05 15:58:01 +0200

    UDATE INVOICES-ROUTING.MODULE
    - Now we have INVOICES-routing module updated with its child en routeee...

--- src/app/invoices/invoices-routing.module.ts
+++ src/app/invoices/invoices-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddInvoicesComponent } from './add-invoices/add-invoices.component';
+import { DeleteInvoicesComponent } from './delete-invoices/delete-invoices.component';
+import { EditInvoicesComponent } from './edit-invoices/edit-invoices.component';
 import { InvoicesComponent } from './invoices.component';
+import { ListInvoicesComponent } from './list-invoices/list-invoices.component';
+import { SearchInvoicesComponent } from './search-invoices/search-invoices.component';
 
-const routes: Routes = [{ path: '', component: InvoicesComponent }];
+const routes: Routes = [
+  { path: '', component: ListInvoicesComponent },
+  { path: 'add', component: AddInvoicesComponent },
+  { path: 'edit/:id', component: EditInvoicesComponent },
+  { path: 'delete/:id', component: DeleteInvoicesComponent },
+  { path: 'search', component: SearchInvoicesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
