c8a2108ff05130da1a2e3d399b33d1e59b565a4d
DemNeg <basingoudembele@gmail.com>
2022-04-05 15:46:59 +0200

    UPDATE CUSTOMERS.MODULES
    -Import and declare customers's children components(add,edit,delete and search.

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
 
 @NgModule({
   declarations: [
     CustomersComponent,
-    ListCustomersComponent
+    ListCustomersComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    DeleteCustomerComponent,
+    SearchCustomerComponent
   ],
   imports: [
     CommonModule,
