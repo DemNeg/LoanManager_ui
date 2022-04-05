f488d919f68ded16b64ec3682092a74645c1ab06
DemNeg <basingoudembele@gmail.com>
2022-04-05 18:52:26 +0200

    UPDATE PAYMENTS.MODULES
    -Import and declare payments.module's children components(add,edit,delete and search).

--- src/app/payments/payments.module.ts
+++ src/app/payments/payments.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { PaymentsRoutingModule } from './payments-routing.module';
 import { PaymentsComponent } from './payments.component';
+import { ListPaymentsComponent } from './list-payments/list-payments.component';
+import { AddPaymentsComponent } from './add-payments/add-payments.component';
+import { EditPaymentsComponent } from './edit-payments/edit-payments.component';
+import { DeletePaymentsComponent } from './delete-payments/delete-payments.component';
+import { SearchPaymentsComponent } from './search-payments/search-payments.component';
 
 
 @NgModule({
   declarations: [
-    PaymentsComponent
+    PaymentsComponent,
+    ListPaymentsComponent,
+    AddPaymentsComponent,
+    EditPaymentsComponent,
+    DeletePaymentsComponent,
+    SearchPaymentsComponent
   ],
   imports: [
     CommonModule,
