1c8bbf876db7becde55d7a8963e2491ac1f3ca13
DemNeg <basingoudembele@gmail.com>
2022-04-05 19:07:17 +0200

    CREATE CHILDREN CMPT FOR PAYMENTS
    - Now we have children components responsible for crud operation on payments and search

---
+++ src/app/payments/add-payments/add-payments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-payments',
+  templateUrl: './add-payments.component.html',
+  styleUrls: ['./add-payments.component.css']
+})
+export class AddPaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/payments/search-payments/search-payments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-payments',
+  templateUrl: './search-payments.component.html',
+  styleUrls: ['./search-payments.component.css']
+})
+export class SearchPaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
