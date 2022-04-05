3d5db46f604e17a219f3b747284cf021fa05d043
DemNeg <basingoudembele@gmail.com>
2022-04-05 18:50:28 +0200

    UDATE PAYMENTS-ROUTING.MODULE
    - Now we have payments-routing module updated with its children routes.

--- src/app/payments/payments-routing.module.ts
+++ src/app/payments/payments-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPaymentsComponent } from './add-payments/add-payments.component';
+import { EditPaymentsComponent } from './edit-payments/edit-payments.component';
+import { ListPaymentsComponent } from './list-payments/list-payments.component';
 import { PaymentsComponent } from './payments.component';
+import { SearchPaymentsComponent } from './search-payments/search-payments.component';
 
-const routes: Routes = [{ path: '', component: PaymentsComponent }];
+const routes: Routes = [
+  { path: '', component: ListPaymentsComponent },
+  { path: 'add', component: AddPaymentsComponent },
+  { path: 'edit/:id', component: EditPaymentsComponent },
+  { path: 'delete/:id', component: EditPaymentsComponent },
+  { path: 'search', component: SearchPaymentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
