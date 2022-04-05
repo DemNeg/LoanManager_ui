e1e87a9f24a7c1e5dcbb83d9cedb03b31e819ee0
DemNeg <basingoudembele@gmail.com>
2022-04-05 16:01:51 +0200

    UPDATE INVOICES.MODULES
    -Import and declare INVOICE's children components(add,edit,delete and search).

--- src/app/invoices/invoices.module.ts
+++ src/app/invoices/invoices.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { InvoicesRoutingModule } from './invoices-routing.module';
 import { InvoicesComponent } from './invoices.component';
+import { ListInvoicesComponent } from './list-invoices/list-invoices.component';
+import { AddInvoicesComponent } from './add-invoices/add-invoices.component';
+import { EditInvoicesComponent } from './edit-invoices/edit-invoices.component';
+import { DeleteInvoicesComponent } from './delete-invoices/delete-invoices.component';
+import { SearchInvoicesComponent } from './search-invoices/search-invoices.component';
 
 
 @NgModule({
   declarations: [
-    InvoicesComponent
+    InvoicesComponent,
+    ListInvoicesComponent,
+    AddInvoicesComponent,
+    EditInvoicesComponent,
+    DeleteInvoicesComponent,
+    SearchInvoicesComponent
   ],
   imports: [
     CommonModule,
