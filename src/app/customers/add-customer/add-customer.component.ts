11711f5f090ed5f94d42f090f9c1eeebac4eb0d6
DemNeg <basingoudembele@gmail.com>
2022-04-07 19:17:05 +0200

    CREATE A FORM FOR CUSTOMER REGISTRATION
    - RAS

--- src/app/customers/add-customer/add-customer.component.ts
+++ src/app/customers/add-customer/add-customer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Customer } from 'src/app/models/customer.model';
+import { CustomerService } from 'src/app/services/customer.service';
+import { CustomerGuardGuard } from '../customer-guard.guard';
 
 @Component({
   selector: 'app-add-customer',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCustomerComponent implements OnInit {
 
-  constructor() { }
+  addCustomerForm!: FormGroup
+  newCustomer?:Customer;
+
+  constructor(private fb:FormBuilder, private customerGuardGuard:CustomerGuardGuard,
+    private router:Router,private customerService:CustomerService) { }
 
   ngOnInit(): void {
+   this.customerGuardGuard.success = false;
+    this.createForm();
+  }
+
+  createForm(){
+   this.addCustomerForm = this.fb.group({
+      firstName: ['',Validators.required],  
+      lastName: [''],  
+      phoneNumber: ['',Validators.required],  
+      emailAddress: [''],  
+      dob: [''],  
+      department: [''] 
+    })
+  }
+
+  onSaveCustomer(){
+    this.customerService.saveCustomer(this.addCustomerForm.value).subscribe(data=>{
+      this.customerGuardGuard.success = true;
+      this.router.navigateByUrl('/customers/add/success');
+    },err=>{
+      console.log(err);
+    });
+    
+    
   }
 
 }
