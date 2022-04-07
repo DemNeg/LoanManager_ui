e3ebdf50378a94b6ed42e9d2d089e62a59c0b2fa
DemNeg <basingoudembele@gmail.com>
2022-04-07 19:23:23 +0200

    UPDATE CUSTOMERS-ROUTING.MODULE
    - WE HAVE CUSTOMERS ROUTING MODULE UPDATED WITH CHILDREN ROUTES AND ROUTEGARDS.

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCustomerComponent } from './add-customer/add-customer.component';
-import { CustomersComponent } from './customers.component';
+import { AddcustomerSuccessComponent } from './add-customer/addcustomer-success/addcustomer-success.component';
+import { CustomerGuardGuard } from './customer-guard.guard';
 import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
 import { EditCustomerComponent } from './edit-customer/edit-customer.component';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
 import { SearchCustomerComponent } from './search-customer/search-customer.component';
+import { ViewCustomerComponent } from './view-customer/view-customer.component';
 
 const routes: Routes = [
   { path: '', component: ListCustomersComponent },
-  { path: 'add', component: AddCustomerComponent },
+  { 
+    path: 'add', 
+    children: [
+      { path:'',component: AddCustomerComponent},
+      { 
+        path: 'success',
+        canActivate:[CustomerGuardGuard],
+        component:AddcustomerSuccessComponent
+      }
+    ]
+  },
   { path: 'edit/:id', component: EditCustomerComponent },
+  { path: 'view/:id', component: ViewCustomerComponent },
   { path: 'delete/:id', component: DeleteCustomerComponent },
   { path: 'search', component: SearchCustomerComponent },
 ];
