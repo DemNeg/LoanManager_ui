60788f13da78d36b7bd4756ec026aa75e533fffc
DemNeg <basingoudembele@gmail.com>
2022-04-07 19:30:14 +0200

    UPDATE DELETE CUSTOMER COMPONENT
    - RAS

--- src/app/customers/delete-customer/delete-customer.component.ts
+++ src/app/customers/delete-customer/delete-customer.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-delete-customer',
   templateUrl: './delete-customer.component.html',
   styleUrls: ['./delete-customer.component.css']
 })
-export class DeleteCustomerComponent implements OnInit {
+export class DeleteCustomerComponent {
+  /*customerId: string = '';
+  resultData: any;
+  deleteCustomerDialogue:boolean=false;
+  
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log("Hello");
+    
+    this.activatedRoute.params.subscribe(data =>{
+      this.customerId = data.id
+      if (this.customerId) {
+        this.deleteCustomerPop();
+      }
+    })
   }
 
+  deleteCustomerPop(){
+    this.deleteCustomerDialogue= true
+  }
+
+  deleteCustomerConfirm(){
+    console.log("sucess ...");
+    
+  }*/
 }
